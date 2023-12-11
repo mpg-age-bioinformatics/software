a10a82b9a7e28c3f1c226416cb05d39a74165eba
Jorge Boucas <jboucas@age.mpg.de>
2023-12-11 08:54:03 +0000

    mageck/e94a4f7

---
+++ mageck/e94a4f7/.command.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/Rscript
+library(MAGeCKFlute)
+library(ggplot2)
+FluteRRA("/AJ02_Lib1_Lib2.gene_summary.txt", "/AJ02_Lib1_Lib2.sgrna_summary.txt", proj="AJ02_Lib1_Lib2", organism="hsa", outdir="/flute_out" )
